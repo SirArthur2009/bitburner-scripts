d5b88da083e495e6186dd6a5784d586945cdf8fc
SirArthur09 <levigbryan@gmail.com>
2025-08-13 10:38:03 -0600

    Create early-hack-template.js

---
+++ early-hack-template.js
@@ -0,0 +1,18 @@
+/** @param {NS} ns **/
+export async function main(ns, hackTarget) {
+    const target = hackTarget || "n00dles"; // fallback
+    ns.tprint(`🚀 Running hack script on target: ${target}`);
+
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.8;
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+    while(true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
+}
